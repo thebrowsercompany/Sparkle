dab43bb6e9db902fb53d9b55f82a1940eae09c62
Renato Mangini <mangini@gmail.com>
2025-12-03 11:57:52 -0700

    change package.swift download url

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let version = "2.8.1-bcny"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
 let tag = "2.8.1-bcny"
 let checksum = "00d0bf1d173a2b124c1abdbaae5aaf9f8a8550274c88be5c5b82291c05551121"
-let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
+let url = "https://github.com/thebrowsercompany/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
 let package = Package(
     name: "Sparkle",
